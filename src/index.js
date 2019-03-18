71e4169db54b07074aaf4132ca19a025feb47b84
AnnaLoiko <amromany@gmail.com>
2019-03-18 16:40:26 +0300

    feat: add getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zerosCount = Infinity;
+
+  for ( let i = 2; i <= base; i++ ) {
+    if ( base % i !== 0 ) continue;
+    
+    let simpleMultiplier = i;
+    let countSimpleMultiplier = 0;
+    let sum = 0;
+    let summand = Math.floor( number / simpleMultiplier );
+    
+    while ( base % simpleMultiplier === 0 ) {
+      base = base / simpleMultiplier;
+      countSimpleMultiplier++; 
+    };
+  
+    while ( summand > 0 ) {
+      sum = sum + summand;
+      summand = Math.floor( summand / simpleMultiplier );
+    };
+
+    currentZeroCount = Math.floor( sum / countSimpleMultiplier );
+    zerosCount = ( zerosCount > currentZeroCount ) ? currentZeroCount : zerosCount;
+  };
+
+  return zerosCount;
 }
